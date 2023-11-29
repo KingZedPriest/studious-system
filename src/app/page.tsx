08762d61fcd4aa905ed472b64b52b82a1ced5f02
KingZedPriest <charleschukwuemeka47@gmail.com>
2023-11-29 18:16:54 +0100

    Feat: Build Logo Area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import Heading from "@/components/landingPageComponents/Heading"
+//Import Needed Components
+import Heading from "@/components/landingPageComponents/Heading";
+import LogoArea from "@/components/landingPageComponents/LogoArea";
 export default function Home() {
   return (
     <main className="">
       <Heading />
+      <LogoArea />
        <div className='max-w-8xl mx-auto px-4 sm:px-6 lg:px-8'>
 
        </div>
---
+++ src/components/landingPageComponents/LogoArea.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+//Import Icons
+import { IoSearchSharp } from "react-icons/io5";
+//Import needed Images
+import logo from "../../../public/logo.png";
+const LogoArea = () => {
+    return ( 
+        <main className="bg-bgBlack px-4 flex justify-between h-[50px] overflow-x-auto bar gap-x-10">
+            <div className="flex gap-x-2 shrink-0">
+                <Image src={logo} alt="logo" className="w-8 md:w-10" />
+                <p className="font-black text-textWhite my-auto text-lg sm:text-xl lg:text-2xl">coin<span className="font-light">codex</span></p>
+            </div>
+            <div className="w-80 shrink-0 border-l border-lightBlue flex justify-between items-center">
+                <input type="text" name="search" id="search" className="outline-0 bg-inherit w-[90%] h-full placeholder:center placeholder:text-sm px-8 text-textWhite placeholder:text-textBlue" placeholder="Search for coins, ICOs and exchanges"/>
+                <IoSearchSharp className="text-textBlue" size={20}/>
+            </div>
+        </main>
+     );
+}
+ 
+export default LogoArea;
