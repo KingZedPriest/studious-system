f2af55268dd689add8ff014bca3f161ed1eab331
KingZedPriest <charleschukwuemeka47@gmail.com>
2023-11-30 20:22:50 +0100

    Feat: Add video using cloudinary

--- src/components/landingPageComponents/Video.tsx
+++ src/components/landingPageComponents/Video.tsx
@@ -1,13 +1,19 @@
-import Video from 'next-video';
+"use client";
+import { CldVideoPlayer } from "next-cloudinary";
+import "next-cloudinary/dist/cld-video-player.css";
+
 //Import Images and Videos
-import introVideo from "../../../videos/introVideo.mp4";
 
 const VideoSection = () => {
-    return ( 
-        <main className='mt-10 px-4 sm:px-6 lg:px-8'>
-            <Video src={introVideo} className='h-96'/>
-        </main>
-     );
-}
- 
+  return (
+    <main className="my-10 px-4 sm:px-6 lg:px-8 rounded-lg w-[80%]">
+      <CldVideoPlayer
+        width="600"
+        height="50"
+        src="video/fkn2qz2akjz8bncai0y7"
+      />
+    </main>
+  );
+};
+
 export default VideoSection;
