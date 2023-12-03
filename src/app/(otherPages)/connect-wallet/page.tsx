e41a24e9706e57e147bf689db7173c6775342301
KingZedPriest <charleschukwuemeka47@gmail.com>
2023-12-03 08:40:16 +0100

    Feat: Add the header

--- src/app/(otherPages)/connect-wallet/page.tsx
+++ src/app/(otherPages)/connect-wallet/page.tsx
@@ -1,7 +1,11 @@
+//Import Components 
+import Heading from "@/components/connectWalletComponents/Heading";
 const page = () => {
     return ( 
         <main className="h-screen w-full">
-            
+            <div className="max-w-[90rem] mx-auto bg-white">
+                <Heading />
+            </div>
         </main>
      );
 }
--- src/components/connectWalletComponents/Heading.tsx
+++ src/components/connectWalletComponents/Heading.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+
+//Import Images
+import logo from "../../../public/logo.png"
 const Heading = () => {
     return ( 
-        <main>
-            
+        <main className="flex justify-between items-center py-4 px-4 sm:px-6 lg:px-8">
+            <Image src={logo} alt="logo" className="w-10 sm:w-14 md:w-18"/>
+            <button className="bg-accentBlue rounded-md p-3 font-bold border-2 border-accentBlue hover:bg-inherit hover:text-accentBlue duration-500">Connect Wallet</button>
         </main>
      );
 }
