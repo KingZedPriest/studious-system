ad57fea45fd725f514525c13d2405f26b558a15d
KingZedPriest <charleschukwuemeka47@gmail.com>
2023-12-06 09:07:47 +0100

    Add the Swap button section

--- src/app/(otherPages)/connect-wallet/page.tsx
+++ src/app/(otherPages)/connect-wallet/page.tsx
@@ -1,12 +1,14 @@
 
 //Import Components 
-
 import Heading from "@/components/connectWalletComponents/Heading";
+import SwapBtnSection from "@/components/connectWalletComponents/SwapBtnSection";
+
 const page = () => {
     return ( 
         <main className="h-screen w-full">
-            <div className="max-w-[90rem] mx-auto bg-white">
+            <div className="max-w-[60rem] mx-auto bg-white">
                 <Heading />
+                <SwapBtnSection />
             </div>
         </main>
      );
