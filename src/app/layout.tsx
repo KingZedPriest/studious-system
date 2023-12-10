51e8cb65598480c36490c2edbee7a2dc5b59413f
KingZedPriest <charleschukwuemeka47@gmail.com>
2023-12-10 04:56:33 +0100

    Feat: Add Whatsapp Icon and the chat Icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Toaster } from "sonner";
 import { Web3Modal } from "@/context/Web3Modal";
 import Whatsapp from "@/components/Whatsapp";
+import LiveChat from "@/components/LiveChat";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -27,6 +28,7 @@ export default function RootLayout({
         <body
           className={`bg-bgBlue text-sm text-textWhite ${roboto.className}`}
         > <Whatsapp />
+          <LiveChat />
           <Web3Modal>{children}</Web3Modal>
           <Toaster richColors position="top-center" closeButton />
         </body>
---
+++ src/components/LiveChat.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { useEffect } from "react";
+const LiveChat = () => {
+    useEffect(() => {
+        window.chatwootSettings = {
+          hideMessageBubble: false,
+          position: "right", 
+          locale: "en", 
+          type: "standard" 
+        };
+          
+        (function(d,t) {
+          var BASE_URL = "https://app.chatwoot.com";
+          var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+          g.src = BASE_URL + "/packs/js/sdk.js";
+          g.async=!0;
+          s.parentNode.insertBefore(g, s);
+    
+          g.onload = function() {
+            window.chatwootSDK.run({
+              websiteToken: process.env.REACT_APP_WEBSITE_TOKEN,
+              baseUrl: BASE_URL
+            })
+          }
+        })(document, "script");
+      }, []);
+    
+      return null;
+}
+ 
+export default LiveChat;
